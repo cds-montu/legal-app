af320bf2c9e99f3d34778bd6d481e6fda07a352a
btwitzmontu <montu.prajapati@coozmoo.com>
2024-09-10 19:21:30 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css'; 
-import Stepper from './Components/autodraftStepper/Stepper';
+import Navbar from './Components/navigationBar/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <Stepper />
+      {/* <Stepper /> */}
+      <Navbar/>
     </div>
   );
 }
